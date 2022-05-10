99fc392cc6aa25580299d4d9ac7877d74c28723a
lanxenet <lanxenet@gmail.com>
2022-05-10 16:38:24 +0800

    geth: 回退版本

--- etherchain/geth.sh
+++ etherchain/geth.sh
@@ -9,7 +9,7 @@ if [ -z "$1" ]; then
 fi
 
 # 指定 geth 版本
-GETH_VERSION=${GETH_VERSION:-geth-linux-amd64-1.10.17-25c9b49f}
+GETH_VERSION=${GETH_VERSION:-geth-linux-amd64-1.10.15-8be800ff}
 
 
 mkdir -p /data/eth
