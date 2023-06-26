af24728f4039f34720d8a007ec87999f18460ecb
enoch <lanxenet@gmail.com>
2023-06-26 14:10:30 +0800

    fix: 更新文档

--- k6/hello.js
+++ k6/hello.js
@@ -1,25 +1,27 @@
+// k6 run --vus 1000 --duration 30s hello.js
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-// export default function () {
-//   http.get('http://220.160.54.46:81/index');
-// }
-
 export default function () {
-  var url = 'http://220.160.54.46:81/search';
-  var payload = JSON.stringify({
-    logname: 'aaa',
-    pwd: '0cc175b9c0f1b6a831c399e269772661',
-    ksh: '12345677',
-    check: 'dddd',
-  });
-
-  var params = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
-
-  const res = http.post(url, payload, params);
+  const res = http.get('http://172.31.23.222/100k.img');
   check(res, { 'status was 200': (r) => r.status == 200 });
 }
+
+// export default function () {
+//   var url = 'http://220.160.54.46:81/search';
+//   var payload = JSON.stringify({
+//     logname: 'aaa',
+//     pwd: '0cc175b9c0f1b6a831c399e269772661',
+//     ksh: '12345677',
+//     check: 'dddd',
+//   });
+
+//   var params = {
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//   };
+
+//   const res = http.post(url, payload, params);
+//   check(res, { 'status was 200': (r) => r.status == 200 });
+// }
