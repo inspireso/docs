4cbb0b126e42d30698a6595c80ab9c7e0922a885
enoch <lanxenet@gmail.com>
2023-10-16 17:19:56 +0800

    feat(python): add selenium demo

---
+++ python/src/selenium/orclib_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+
+import os
+
+from ocrlib import OcrByOnnx
+
+
+def test():
+    ocr = OcrByOnnx()
+
+    # 获取当前文件目录
+    current_path = os.path.dirname(__file__)
+
+    with open(os.path.join(os.path.dirname(__file__), 'testdata/p1.png'), 'rb') as f:
+        img_bytes = f.read()
+
+    # 识别图片中的文字
+    res = ocr.classification(img_bytes)
+
+    print(res)
